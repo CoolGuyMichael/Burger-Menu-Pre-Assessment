1ecb62bf9ee512cec1a8c71861c4f6e678256753
CoolGuyMichael <chium@middleton.school.nz>
2026-03-31 09:28:10 +1300

    component 1a

---
+++ Burger_menu_v1.py
@@ -0,0 +1,27 @@
+"""
+Program to store combo meals for a takeway business
+26-3-26
+Version 1: Set up main program and while loop
+"""
+
+burger_menu = {
+    "Value" : {
+        "Main" : "Beef burger",
+        "Side" : "Fries",
+        "Drink" : "Fizzy drink"
+    },
+    "Cheezy" : {
+        "Main" : "Cheeseburger",
+        "Side" : "Fries",
+        "Drink" : "Fizzy drink"
+    },
+    "Super" : {
+        "Main" : "Cheeseburger",
+        "Side" : "Large fries",
+        "Drink" : "Smoothie"
+    }
+}
+
+cool = input("Here: ").title()
+print (f"{cool}")
+
